f75a5be57257e6a3ba6a25c8783a55ad9c50986a
johannes mokibelo Mante <125446000+mantejohannes@users.noreply.github.com>
2023-10-18 08:19:40 +0200

    responsive portfolio with php

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+
+$recipient = "mokibelomante@gmail.com";
+
+mail($recipient. $subject. $message $mailheader)
+or die("Error!");
+
+echo"message send";
+
+?>
